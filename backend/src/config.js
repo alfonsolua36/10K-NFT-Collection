48e328c5909b7ab54625e5af0f834bb5b9ce2803
Alfonso Lua <49700392+alfonsolua36@users.noreply.github.com>
2022-09-13 19:58:34 -0700

    html changes

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,7 +7,7 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Sodahead";
+const namePrefix = "Sodaheads";
 const description = "Because Soda is Tasty";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
@@ -49,7 +49,7 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'Sodahead';
+const CONTRACT_NAME = 'Sodaheads';
 const CONTRACT_SYMBOL = 'SHD';
 const METADATA_UPDATABLE = false; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x40b4CB12AF70CC0a6a7292001C0fe5902666D2F1';
